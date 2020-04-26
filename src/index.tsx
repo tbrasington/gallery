386911741f62b5df937abfaed34d1cc6ba8a0646
Thomas Brasington <thomas@66north.is>
2020-04-26 20:38:54 +0100

    prep for animation + rollup

--- src/index.tsx
+++ src/index.tsx
@@ -1,61 +1,92 @@
 import { wrap } from "@popmotion/popcorn"
+import { AnimatePresence, motion } from "framer-motion"
 import React, { useState } from "react"
+import styles from "./index.css"
 
 const Gallery: React.FC<{
-
   children?: React.ReactNode
-
 }> = ({ children }) => {
 
-  const originalChildren =  React.Children.toArray(children)
+  const originalChildren = React.Children.toArray(children)
   const slideTotal = React.Children.count(children)
-
-  const originalNodeArray = originalChildren.map((item: any, index: number)=> ({"key"  : item.key, "position" : index}))
-
+  const originalNodeArray = originalChildren.map((item: any, index: number) => ({ "key": item.key, "position": index }))
   const mutatedNodeArray = originalNodeArray.map((x) => x)
-  //mutatedNodeArray.splice(0,0,originalNodeArray[slideTotal-1])
-  //mutatedNodeArray.pop()
   const showSlices = 3
-  
-  mutatedNodeArray.splice(0, 0, originalNodeArray[slideTotal-1])
-  mutatedNodeArray.splice(slideTotal+1, 0, originalNodeArray[0])
-  console.info("mutated", mutatedNodeArray)
+
+  // first tree to show
+  mutatedNodeArray.splice(0, 0, originalNodeArray[slideTotal - 1])
+  mutatedNodeArray.splice(slideTotal + 1, 0, originalNodeArray[0])
 
   // state
-  const [{slide, direction,  tree}, setPosition] = useState({
-      slide : 0, 
-      direction : 0, 
-      tree : mutatedNodeArray.slice(0,showSlices)
-   })
-  
+  const [{ slide, direction, tree }, setPosition] = useState({
+    slide: 0,
+    direction: 0,
+    tree: mutatedNodeArray.slice(0, showSlices)
+  })
+
   const imageIndex = wrap(0, slideTotal, slide)
 
+  const containerWidth = window.innerWidth
+  const slideWidth = containerWidth *.8
   // events
   const paginate = (newDirection: number) => {
-
- 
-    
     const treeIndex = wrap(0, slideTotal, slide + newDirection)
-   
-    setPosition({
-      slide : slide + newDirection, 
-      direction : newDirection
-      , tree :   mutatedNodeArray.slice(treeIndex ,treeIndex + showSlices)
-    })
 
+    setPosition({
+      slide: slide + newDirection,
+      direction: newDirection
+      , tree: mutatedNodeArray.slice(treeIndex, treeIndex + showSlices)
+    })
+  }
+
+  // motion
+  const variants = {
+    enter: (direction: number) => {
+      return {
+        x: direction > 0 ? window.innerWidth * 0.9 : -window.innerWidth * 0.9,
+        opacity: 0,
+      }
+    },
+    center: {
+      x: (((slideWidth * slideTotal) - containerWidth) / 2) *-1,
+      opacity: 1,
+    },
+    exit: (direction: number) => {
+      return {
+        x: direction < 0 ? window.innerWidth * 0.9 : -window.innerWidth * 0.9,
+        opacity: 0,
+      }
+    }
   }
 
   return (
-    <div>
-      <div style={{display:"flex", flexDirection:"column"}}>
-      {tree.map((node,index)=> {
-        return(<div key={index} style={{
-          
-        }}>{ (node.position === imageIndex ? "🟢" : "🔵") +  node.key}</div>) 
-      })}
-      </div>
+    <div className={styles.container}>
+
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.div 
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 200, damping: 200 },
+            opacity: { duration: 0.7 },
+          }}
+          className={styles.track} style={{
+            width: `${slideTotal * 100}%`
+          }}>
+          {tree.map((node, index) => {
+            return (
+              <div key={index} className={styles.slide}>
+                {(node.position === imageIndex ? "🟢" : "🔵") + node.key}
+              </div>)
+          })}
+        </motion.div>
+      </AnimatePresence>
+
       <div>{imageIndex}</div>
-      <div></div>
+
       <div>
         <button onClick={() => paginate(-1)}>Previous</button>
         <button onClick={() => paginate(1)}>Next</button>
@@ -64,11 +95,4 @@ const Gallery: React.FC<{
 }
 
 export default Gallery
-
-  /* 
-  
-    todo:
-
-    expose controls to be custom in appearance and placement
-
-  */
+ 
